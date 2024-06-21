0bbae99e254b3f3bbb9632dd8383bc89afb5a1aa
Victor Okoli <147929336+vokoli10@users.noreply.github.com>
2024-06-21 10:02:57 +0100

    exec script.

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,7 +1,6 @@
-#/bin/bash
+#/!bin/bash
 sudo apt install cowsay -y
 cowsay -f dragon "Run for cover, I am a DRAGON .....RAWR!">> dragon.txt
 grep -i "dragon" dragon.txt
 run: cat dragon.txt
 ls -ltra
-
